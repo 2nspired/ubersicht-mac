f5fde790c16eadaede3e4392c7fe967870f2bdcf
Thomas Trudzinski <thomastrudzinski@gmail.com>
2024-04-05 11:02:20 -1000

    Added scraper for twitch metrics: userName, rank, rankPercentile, followers, avgViewers

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ For general info check out the [Übersicht website.](http://tracesof.net/uebersi
 ---
 
 github: [https://github.com/felixhageloh/uebersicht](https://github.com/felixhageloh/uebersicht)
+
+// Setting up Twitch Integration
+1. Register a new app on twitch: https://dev.twitch.tv/docs/authentication/register-app/
+
+
+2. Install Twitch CLI: https://dev.twitch.tv/docs/cli/
+- You will need to download this so obtain your auth token.
+Use command: $ twitch configure, enter your apps Client Id and Client Secret. 
+
+3. Get an access token: https://dev.twitch.tv/docs/cli/token-command/
+- Use the token command to get an access token. Twitch API endpoints require an app access token or a user access token (see the endpoint’s reference content for token requirements).
+
--- crypto/app.jsx
+++ crypto/app.jsx
@@ -2,50 +2,50 @@
 // crypto-app-jsx
 // -------------------------------------------------------------
 
-import { css, styled } from 'uebersicht';
+import { css, styled } from "uebersicht";
 
 export const refreshFrequency = 1000 * 60 * 15; // time in ms
 
 const coinAPI = {
-    url: 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest',
-    key: 'API-KEY',
-    currency: 'USD'
+  url: "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest",
+  key: "API-KEY",
+  currency: "USD",
 };
 
-const uberProxy = 'http://127.0.01:41417';
+const uberProxy = "http://127.0.01:41417";
 
 const coinList = [
-    { symbol: 'BTC', isActive: true },
-    { symbol: 'ETH', isActive: true },
-    { symbol: 'SOL', isActive: true },
-    { symbol: 'ADA', isActive: true },
-    { symbol: 'DOT', isActive: true }
+  { symbol: "BTC", isActive: true },
+  { symbol: "ETH", isActive: true },
+  { symbol: "SOL", isActive: true },
+  { symbol: "ADA", isActive: true },
+  { symbol: "DOT", isActive: true },
 ];
 
-const coinUrl = `${coinAPI.url}?convert=${coinAPI.currency}&symbol=${coinList.map(
-    (coin) => coin.symbol
-)}`;
+const coinUrl = `${coinAPI.url}?convert=${
+  coinAPI.currency
+}&symbol=${coinList.map((coin) => coin.symbol)}`;
 
 // -------------------------------------------------------------
 // command
 // -------------------------------------------------------------
 
 export const command = (dispatch) => {
-    fetch(`${uberProxy}/${coinUrl}`, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'X-CMC_PRO_API_KEY': coinAPI.key
-        }
-    })
-        .then((response) =>
-            response.json().then((json) => {
-                dispatch({ type: 'FETCH_SUCCEDED', data: json.data });
-            })
-        )
-        .catch((error) => {
-            dispatch({ type: 'FETCH_FAILED', error: error });
-        });
+  fetch(`${uberProxy}/${coinUrl}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "X-CMC_PRO_API_KEY": coinAPI.key,
+    },
+  })
+    .then((response) =>
+      response.json().then((json) => {
+        dispatch({ type: "FETCH_SUCCEDED", data: json.data });
+      })
+    )
+    .catch((error) => {
+      dispatch({ type: "FETCH_FAILED", error: error });
+    });
 };
 
 // -------------------------------------------------------------
@@ -55,29 +55,29 @@ export const command = (dispatch) => {
 export const initialState = { coins: [] };
 
 export const updateState = (event, previousState) => {
-    switch (event.type) {
-        case 'FETCH_SUCCEDED': {
-            const data = coinList.map((coin) => {
-                coin.quote = event.data[coin.symbol][0].quote[coinAPI.currency];
-                return coin;
-            });
-            return { coins: data };
-        }
-        case 'FETCH_FAILED': {
-            const data = coinList.map((coin) => {
-                coin.quote = {
-                    percent_change_1h: 0,
-                    percent_change_24h: 0,
-                    price: 0
-                };
-                return coin;
-            });
-            return { coins: data, error: event.error };
-        }
-        default: {
-            return previousState;
-        }
+  switch (event.type) {
+    case "FETCH_SUCCEDED": {
+      const data = coinList.map((coin) => {
+        coin.quote = event.data[coin.symbol][0].quote[coinAPI.currency];
+        return coin;
+      });
+      return { coins: data };
     }
+    case "FETCH_FAILED": {
+      const data = coinList.map((coin) => {
+        coin.quote = {
+          percent_change_1h: 0,
+          percent_change_24h: 0,
+          price: 0,
+        };
+        return coin;
+      });
+      return { coins: data, error: event.error };
+    }
+    default: {
+      return previousState;
+    }
+  }
 };
 
 // -------------------------------------------------------------
@@ -85,62 +85,62 @@ export const updateState = (event, previousState) => {
 // -------------------------------------------------------------
 
 export const className = css`
-    bottom: 15px;
-    left: 20px;
-    font-family: 'Helvetica Neue', sans-serif;
+  bottom: 15px;
+  left: 20px;
+  font-family: "Helvetica Neue", sans-serif;
 `;
 
-const Widget = styled('div')`
-    display: flex;
+const Widget = styled("div")`
+  display: flex;
 `;
 
-const Coin = styled('div')((props) => ({
-    display: props.status ? 'flex' : 'none',
-    flexDirection: 'column',
-    paddingRight: '25px',
-    ':last-child': {
-        paddingRight: '0px'
-    }
+const Coin = styled("div")((props) => ({
+  display: props.status ? "flex" : "none",
+  flexDirection: "column",
+  paddingRight: "25px",
+  ":last-child": {
+    paddingRight: "0px",
+  },
 }));
 
-const SymbolTop = styled('div')`
-    color: #ffffff;
-    font-size: 11px;
-    font-weight: 400;
-    letter-spacing: 0.5px;
-    padding-right: 15px;
-    padding-bottom: 2px;
-    padding-top: 2px;
+const SymbolTop = styled("div")`
+  color: #ffffff;
+  font-size: 11px;
+  font-weight: 400;
+  letter-spacing: 0.5px;
+  padding-right: 15px;
+  padding-bottom: 2px;
+  padding-top: 2px;
 `;
 
-const SymbolBottom = styled('div')`
-    color: #ffffff99;
-    font-size: 11px;
-    font-weight: 400;
-    letter-spacing: 0.5px;
-    padding-right: 15px;
-    padding-bottom: 2px;
-    padding-top: 2px;
+const SymbolBottom = styled("div")`
+  color: #ffffff99;
+  font-size: 11px;
+  font-weight: 400;
+  letter-spacing: 0.5px;
+  padding-right: 15px;
+  padding-bottom: 2px;
+  padding-top: 2px;
 `;
 
-const Price = styled('div')`
-    color: #ffffff;
-    font-size: 16px;
-    font-weight: 400;
-    letter-spacing: 1px;
+const Price = styled("div")`
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 1px;
 `;
 
-const Change = styled('div')`
-    color: #ffffff99;
-    font-size: 11px;
-    font-weight: 400;
-    letter-spacing: 0px;
-    padding-right: 15px;
-    font-family: 'Courier', sans-serif;
+const Change = styled("div")`
+  color: #ffffff99;
+  font-size: 11px;
+  font-weight: 400;
+  letter-spacing: 0px;
+  padding-right: 15px;
+  font-family: "Courier", sans-serif;
 `;
 
-const Spacer = styled('div')`
-    height: 12px;
+const Spacer = styled("div")`
+  height: 12px;
 `;
 
 // -------------------------------------------------------------
@@ -148,24 +148,24 @@ const Spacer = styled('div')`
 // -------------------------------------------------------------
 
 export const render = ({ coins, error }) => {
-    return (
-        <Widget>
-            {coins.map((coin) => (
-                <Coin key={coin.symbol} status={coin.isActive}>
-                    <SymbolTop>{coin.symbol}</SymbolTop>
-                    <Change>
-                        {coin.quote.percent_change_1h > 0 && '\u202F'}
-                        {coin.quote.percent_change_1h.toFixed(2)} 1h
-                    </Change>
-                    <Change>
-                        {coin.quote.percent_change_24h > 0 && '\u202F'}
-                        {coin.quote.percent_change_24h.toFixed(2)} 24h
-                    </Change>
-                    <Spacer />
-                    <SymbolBottom>{coin.symbol}</SymbolBottom>
-                    <Price>{coin.quote.price.toFixed(2)}</Price>
-                </Coin>
-            ))}
-        </Widget>
-    );
+  return (
+    <Widget>
+      {coins.map((coin) => (
+        <Coin key={coin.symbol} status={coin.isActive}>
+          <SymbolTop>{coin.symbol}</SymbolTop>
+          <Change>
+            {coin.quote.percent_change_1h > 0 && "\u202F"}
+            {coin.quote.percent_change_1h.toFixed(2)} 1h
+          </Change>
+          <Change>
+            {coin.quote.percent_change_24h > 0 && "\u202F"}
+            {coin.quote.percent_change_24h.toFixed(2)} 24h
+          </Change>
+          <Spacer />
+          <SymbolBottom>{coin.symbol}</SymbolBottom>
+          <Price>{coin.quote.price.toFixed(2)}</Price>
+        </Coin>
+      ))}
+    </Widget>
+  );
 };
